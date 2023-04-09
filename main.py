9a102eaa4726c1f414f5fc5e9d0f1ff7825e345c
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-08 21:13:52 -0500

    Added main functionality

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+This Add-On computes the SHA-1 hash of a document and saves it as a key/value pair on the document. 
 """
 
-from documentcloud.addon import AddOn
+from documentcloud.addon import SoftTimeOutAddOn
+import hashlib
 
-
-class HelloWorld(AddOn):
+class Hash(AddOn):
     """An example Add-On for DocumentCloud."""
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
-
+            hash = hashlib.sha1(document.pdf).hexdigest()
+            document.data.update({"hash": hash}) 
+            document.save()
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    Hash().main()
